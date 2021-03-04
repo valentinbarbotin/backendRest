5acb199171fc34287d8def514ff4f2062b4ec82c
valentinbarbotin <valentinbarbotin3@gmail.com>
2021-03-04 23:00:18 +0100

    upload

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -11,6 +11,8 @@ const request = require('request');
 const TokenGenerator = require('uuid-token-generator');
 
 exports.index = function (req, res) {
+
+    console.log(req.files)
     res.json({ message: 'Hello !' });
 };
 
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/tmp/scrypteur')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname + '-' + Date.now())
+    }
+  })
+
+
+const upload = multer({ dest: 'uploads/', storage: storage})
 
 const checkIP = require('../middleware/checkIP');
 const Users = require('../controllers/users');
 
+
+router.post('/upload', checkIP, upload.array('file'), Users.index );
 
 router.post('/register', checkIP, Users.register );
 router.post('/login', checkIP, Users.login );
